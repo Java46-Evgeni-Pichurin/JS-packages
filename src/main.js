cb24d9f1608563273e362a2fb6e565f3c2754a65
Evgeni Pichurin <pichurinaa@gmail.com>
2022-03-03 12:47:54 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import courseData from './config/courseData.json'
+import College from './services/college';
+import Courses from './services/courses';
+import FormHandler from './ui/form_handler';
+import TableHandler from './ui/table_handler';
+import { getRandomCourse } from './utils/randomCourse';
+const N_COURSES = 5;
+function createCourses() {
+    const courses = [];
+    for (let i = 0; i < N_COURSES; i++) {
+        courses.push(getRandomCourse(courseData));
+    }
+    return courses;
+}
+
+
+const courses = createCourses();
+
+const dataProvider = new Courses(courseData.minId, courseData.maxId, courses);
+const dataProcessor = new College(dataProvider, courseData);
+const tableHandler = new TableHandler([
+    {key: 'id', displayName: 'ID'},
+    {key: 'name', displayName: 'Course Name'},
+    {key: 'lecturer', displayName: 'Lecturer Name'},
+    {key: 'cost', displayName: "Cost (ILS)"},
+    {key: 'hours', displayName: "Course Duration (h)"}
+], "courses-table");
+const formHandler = new FormHandler("courses-form", "alert");
+formHandler.addHandler(course => {
+    const res = dataProcessor.addCourse(course);
+    if (typeof (res) !== 'string') {
+        return '';
+    }
+    return res;
+
+})
+formHandler.fillOptions("course-name-options", courseData.courses);
+formHandler.fillOptions("lecturer-options", courseData.lectors);
+
+window.showForm = () => {
+    formHandler.show();
+    tableHandler.hideTable();
+}
+window.showCourses = () => {
+    tableHandler.showTable(dataProcessor.getAllCourses());
+    formHandler.hide();
+}
+function selectTitle(id, title){
+    document.getElementById(id).innerHTML += `<option value="${title}" selected>${title}</option>`;
+}
+selectTitle(`course-name-options`, `--Select course name--`);
+selectTitle(`lecturer-options`, `--Select lecture name--`);
